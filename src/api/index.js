a137152937dd3ca5e2ec56b13f590497601e05a0
magz <magzb.99@gmail.com>
2019-05-26 11:30:01 -0400

    [Chore][0.1]: Adding test data and /applications route.

--- src/api/index.js
+++ src/api/index.js
@@ -1,8 +1,12 @@
 const express = require("express");
 
-const { healthRouter } = require('../routes/health/health.router')
+const { healthRouter } = require("../routes/health/health.router");
+const {
+  applicationsRouter,
+} = require("../routes/applications/applications.router");
 
 const router = express.Router();
 router.use("/health", healthRouter);
+router.use("/applications", applicationsRouter);
 
 module.exports = router;
