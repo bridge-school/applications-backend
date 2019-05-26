e923daa3e7e8d8f1274e4c1e4b57dc677c39f724
Magz <magzb.99@gmail.com>
2019-05-26 15:38:21 -0400

    [Feat][0.1]: Adding test data and /applications route. (#2)
    * [Chore][0.1]: Adding test data and /applications route.
    
    * [Feat][0.1]: Adding test data.
    
    * Deleting file!

---
+++ src/routes/applications/applications.test.js
@@ -0,0 +1,9 @@
+const request = require("supertest");
+
+const { applicationsRouter } = require("./applications.router");
+
+describe("tests for the applications check endpoint", () => {
+  it("returns a 200", () => {
+    expect(true).toBe(true);
+  });
+});
