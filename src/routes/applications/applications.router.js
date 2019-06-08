5dfee4d65be63e3ac026a49072b5e36b91d4c734
Corina Wong <hello@corinawong.com>
2019-06-07 22:24:15 -0400

    Add create application route

--- src/routes/applications/applications.router.js
+++ src/routes/applications/applications.router.js
@@ -3,8 +3,11 @@ const express = require('express');
 const { allApplications } = require('./controller/allApplications');
 const { currentApplications } = require('./controller/currentApplications');
 const { singleApplication } = require('./controller/singleApplication');
+const { createApplication } = require('./controller/createApplication');
 
 const router = express.Router();
+
+router.post('/', createApplication);
 
 router.get('/current', currentApplications);
 router.get('/:applicationId', singleApplication); //  /applications/DM7kUT8CCS8tE9wzn9pv
