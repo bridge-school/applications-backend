21b6551e09f9234156cadbcc9f5418959583e0ec
Corina Wong <hello@corinawong.com>
2019-06-15 14:16:04 -0400

    Remove healthRouter

--- src/api/index.js
+++ src/api/index.js
@@ -1,12 +1,10 @@
 const express = require('express');
 
-const { healthRouter } = require('../routes/health/health.router');
 const {
   applicationsRouter,
 } = require('../routes/applications/applications.router');
 
 const router = express.Router();
-router.use('/health', healthRouter);
 router.use('/applications', applicationsRouter);
 
 module.exports = router;
