a9baaf16388104b55f6064f6add2382c9f60ad5e
magz99 <magzb.99@gmail.com>
2019-06-15 15:05:55 -0400

    [Feat]: Refactoring create application to check slug beforehand.

--- src/routes/applications/applications.router.js
+++ src/routes/applications/applications.router.js
@@ -4,13 +4,11 @@ const { allApplications } = require('./controller/allApplications');
 const { currentApplications } = require('./controller/currentApplications');
 const { singleApplication } = require('./controller/singleApplication');
 const { createApplication } = require('./controller/createApplication');
-const { applicationSlug } = require('./controller/applicationSlug');
 
 const router = express.Router();
 
 router.post('/', createApplication);
 
-router.get('/slug/:slug', applicationSlug);
 router.get('/current', currentApplications);
 router.get('/:applicationId', singleApplication); //  /applications/DM7kUT8CCS8tE9wzn9pv
 router.get('/', allApplications);
